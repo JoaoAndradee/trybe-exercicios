42a7565e45576cc841bb80d3deac76a1eb8179ec
joao <joao.dev94@gmail.com>
2022-09-15 01:39:05 -0300

    Aprendendo RTL parte 01

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/day-01-rtl-primeiros-passos/testes-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label htmlFor="id-email">
+        Email
+        <input id="id-email" type="email" />
+      </label>
+      <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
+      <input id="btn-back" type="button" value="Voltar" />
+    </div>
+  );
+}
+
+export default App;
