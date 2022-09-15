9f7770bccdc2c29bd2d829bb413feb2f4530ae9b
joao <joao.dev94@gmail.com>
2022-09-15 20:29:14 -0300

    Resolução de metade dos exercicios, criação dos arquviso funcionais: adicao.js; subtracao.js; multiplicacao.js; divisao.js; modulo.js; angulosTriangulos.js; maiorDeDoisNumeros.js; maiorDeTresNumeros.js; positiveOrNegative.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/PositiveOrNegative.js
@@ -0,0 +1,9 @@
+function PositiveOrNegative(number) { 
+    if (number > 0) {
+        return 'Positivo'
+    } else if (number < 0) {
+        return 'Negativo'
+    } else {
+        return 0;
+    }
+}
