2c6122e6ab5fc90f7103c2196d7337bb36534e31
joao <joao.dev94@gmail.com>
2022-09-15 19:28:28 -0300

    Reiniciando o repositorio, mas mantendo o readme

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/day-01-introducao-a-higher-order-functions/winningTicket.js
+++
@@ -1,9 +0,0 @@
-const conferirAposta = (num1, num2) => (num1 === num2) ? 'Você Ganhou!!' : 'Tente novamente!!';
-
-const sorteio = (numeroApostado, callback) => {
-    const numeroSorteado = Math.round(Math.random() * 4) + 1;
-    console.log(`Você jogou ${numeroApostado} e o número sorteado foi ${numeroSorteado}`);
-    return callback(numeroApostado, numeroSorteado);
-};
-
-console.log(sorteio(2, conferirAposta));
